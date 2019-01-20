e9f860f121960c52575b301e64d6840d6de296aa
dipanjal <dipanjalmaitra@gmail.com>
2019-01-21 00:33:29 +0600

    added pass by agr & count issue fixed

--- docker/wallhaven.py
+++ docker/wallhaven.py
@@ -52,18 +52,37 @@ if args.url:
     keyword = ROOT_URL[start_index:end_index].strip()
     if keyword=='':
         keyword = datetime.datetime.today().strftime('%B %d,%Y')
+    #     temp_path = fixed_path+keyword
+    #     # temp_loc = file_path_to_save.format(search_query=keyword)
+    #     # print('temp:'+temp_loc)
+    #     # temp_path = Path(temp_loc).expanduser()
+    #     count = 0
+    #     print(count,":",temp_path)
+    #     # while os.path.exists(temp_path):
+    #     while (Path(temp_path).expanduser()).exists:
+    #         count = count+1
+    #         keyword=datetime.datetime.today().strftime('%B %d,%Y')+"("+str(count)+")"
+            
+    #         # temp_path = temp_path.replace(keyword,'')
+    #         # print('temp_path: '+temp_path)
+    #         # keyword = re.sub(r"\(\d+\)","("+str(count)+")", keyword)
+    #         temp_path = temp_path+keyword
+    #         print(count,":",temp_path)
+    #         # temp_loc = file_path_to_save.format(search_query=keyword)
+    #         # print('temp:'+temp_loc)
+    #         # temp_path = 
+        
     file_path_to_save = file_path_to_save.format(search_query=keyword)
     path = Path(file_path_to_save).expanduser()
     path.mkdir(parents=True,exist_ok=True)
 elif args.keyword:
-    # print('keyword: ',args.keyword)
+    print('keyword: ',args.keyword)
     file_path_to_save = file_path_to_save.format(search_query=args.keyword)
     if args.ratio:
         print('ratio: ',args.ratio)
         ROOT_URL = ROOT_URL.format(search_query=args.keyword,page_number='{page_number}',screen_ration=args.ratio)
     else:
         ROOT_URL = ROOT_URL.format(search_query=args.keyword,page_number='{page_number}',screen_ration='')
-    # print("search url:",ROOT_URL)
 else:
     print('usage: python3 wallhaven.py -w <keyword>')
 
@@ -79,8 +98,7 @@ def generate_downloadable_image_url(response,session):
     divs = soup.find_all('figure', attrs={'class': 'thumb'})
     if len(divs) < 1:
         print("No More Image Found Here! ")
-        # sys.exit()
-        return False
+        sys.exit()
     else:
         print("Download Path: "+file_path_to_save)
         path = Path(file_path_to_save).expanduser()
@@ -89,23 +107,17 @@ def generate_downloadable_image_url(response,session):
         for div in divs:
             pass
             wall_id = div['data-wallpaper-id']
-            print(wall_id+'.jpg')
+            print(wall_id)
             img_response = session.get('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{iid}.jpg'.format(iid=wall_id), stream=True)
             with open(path/Path(wall_id + '.jpg'), 'wb') as f:
                 for chunk in img_response.iter_content():
                     if chunk:
                         f.write(chunk)
                 total_downloaded+=1
-        return True
-
-
 
 with requests.Session() as session:
     session.headers.update({'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'})
-    request_url = ROOT_URL.format(page_number=1)
-    # print('requesting_url:',request_url)
-    response = session.get(request_url)
-    # print(response)
+    response = session.get(ROOT_URL.format(page_number=1))
     if response.ok:
         soup = BeautifulSoup(response.text, 'html.parser')
         divs = soup.find_all('figure', attrs={'class': 'thumb'})
@@ -114,20 +126,12 @@ with requests.Session() as session:
             print("No Image Found!")
             sys.exit()
         
-        # count = soup.find('h2').text.split("/")[-1].strip()
-        # print("Total Page Count: ",soup.find('h2'))
-        page_number = 1
-        flag = True
-        while flag:
+        count = soup.find('h2').text.split("/")[-1].strip()
+        print("Total Page Count: "+count)
+        for page_number in range(1,int(count)+1):
             current_page_url=ROOT_URL.format(page_number=page_number)
             response = session.get(current_page_url)
             print(current_page_url)
-            flag = generate_downloadable_image_url(response,session)
-            page_number+=1
-        # for page_number in range(1,int(count)+1):
-        #     current_page_url=ROOT_URL.format(page_number=page_number)
-        #     response = session.get(current_page_url)
-        #     print(current_page_url)
-        #     # generate_downloadable_image_url(response,session)
+            generate_downloadable_image_url(response,session)
         print("Total Downloaded: ",total_downloaded)
 
